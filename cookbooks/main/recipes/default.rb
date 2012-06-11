4dfc25d39fe5820f3e73a2887186b1b7bcb16b30
linc01n <git@lincoln.hk>
2012-06-12 00:53:45 +0800

    Initial commit with chef base

---
+++ cookbooks/main/recipes/default.rb
@@ -0,0 +1,16 @@
+package "git-core"
+package "python-software-properties"
+package "zsh"
+
+user node["user"]["name"] do
+  password node["user"]["password"]
+  gid "admin"
+  home "/home/#{node["user"]["name"]}"
+  supports manage_home: true
+  shell "/bin/zsh"
+end
+
+template "/home/#{node["user"]["name"]}/.zshrc" do
+  source "zshrc.erb"
+  owner node["user"]["name"]
+end
---
+++ solo.rb
@@ -0,0 +1,3 @@
+cookbook_path File.expand_path("../cookbooks", __FILE__)
+json_attribs File.expand_path("../node.json", __FILE__)
+
