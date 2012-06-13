cc415a23d000679dcee1ad84918b2da64312ca65
Lincoln Lee <git@lincoln.hk>
2012-06-13 12:44:27 +0800

    Add opencc and imagemagick
    Signed-off-by: Lincoln Lee <git@lincoln.hk>

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -40,3 +40,25 @@ end
 include_recipe "apt"
 
 package "nodejs"
+
+package "imagemagick"
+
+#opencc
+package "cmake"
+package "gettext"
+remote_file "/home/deployer/opencc-0.3.0.tar.gz" do
+  source "http://opencc.googlecode.com/files/opencc-0.3.0.tar.gz"
+  action :create_if_missing
+end
+
+bash "compile_opencc" do
+  cwd "/home/deployer"
+  code <<-EOH
+    tar zxf opencc-0.3.0.tar.gz
+    cd opencc-0.3.0
+    mkdir build
+    cd build
+    cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -D ENABLE_GETTEXT:BOOL=ON ..
+    make & make install
+  EOH
+end
