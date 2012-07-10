b2f68e99b5ef93ff4bc6305f2f01281a48cbddd9
Lincoln Lee <git@lincoln.hk>
2012-07-10 12:45:41 +0800

    Make basic system template
    Signed-off-by: Lincoln Lee <git@lincoln.hk>

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -7,13 +7,9 @@ package "emacs23-nox"
 include_recipe "nginx::source"
 
 
-group "admin" do
-  gid 111
-end
-
 user node["user"]["name"] do
   password node["user"]["password"]
-  gid "admin"
+  gid "adm"
   home "/home/#{node["user"]["name"]}"
   supports manage_home: true
   shell "/bin/zsh"
@@ -22,16 +18,12 @@ end
 template "/home/#{node["user"]["name"]}/.zshrc" do
   source "zshrc.erb"
   owner node["user"]["name"]
-  group "admin"
+  group "adm"
 end
 
 service "nginx" do
   action :start
 end
-
-package "mysql-server"
-package "mysql-client"
-package "libmysqlclient-dev"
 
 execute "add-apt-repository ppa:chris-lea/node.js" do
   action :run
@@ -42,25 +34,3 @@ include_recipe "apt"
 package "nodejs"
 
 package "imagemagick"
-
-#opencc
-package "cmake"
-package "gettext"
-remote_file "/home/deployer/opencc-0.3.0.tar.gz" do
-  owner "deployer"
-  source "http://opencc.googlecode.com/files/opencc-0.3.0.tar.gz"
-  action :create_if_missing
-end
-
-bash "compile_opencc" do
-  user "deployer"
-  cwd "/home/deployer"
-  code <<-EOH
-    tar zxf opencc-0.3.0.tar.gz
-    cd opencc-0.3.0
-    mkdir build
-    cd build
-    cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -D ENABLE_GETTEXT:BOOL=ON ..
-    make & sudo make install
-  EOH
-end
